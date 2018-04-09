17be3609074d7302cab4eaf0bd41847b388107db
centrevillage <centrevillage@gmail.com>
2018-04-09 20:43:18 +0900

    cleanup

--- app.rb
+++ app.rb
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 
-#require 'active_support'
-#require 'active_support/core_ext'
-
 require 'action_mailer'
 require 'letter_opener'
 
-AppRoute ||= Pathname.new(File.expand_path(File.dirname(__FILE__)))
-
 def setup_mailer
-  ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, location: AppRoute.join('tmp/letter_opener').to_s
+  ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, location: 'tmp/letter_opener'
   ActionMailer::Base.delivery_method = :letter_opener
 end
 
@@ -30,7 +25,6 @@ class NewsMailer < ActionMailer::Base
 end
 
 if $0 === __FILE__
-  AppEnv = ENV['APP_ENV'].presence || 'development'
   setup_mailer
   send_mail
 end
